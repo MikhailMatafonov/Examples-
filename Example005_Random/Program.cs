23ebd6dea5018f2dadc6ee2e055ae613be8143c9
Михаил Матафонов <Matmix2015@yandex.ru>
2023-02-13 15:53:51 +0300

    Random Sum

---
+++ Example005_Random/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next (1, 10); // 1 2 3 4 ...9
+Console.WriteLine ("Число А = "+numberA);
+int numderB = new Random().Next (1, 10);
+Console.WriteLine ("Число В = "+numderB);
+int result = numberA + numderB;
+Console.WriteLine ("Сумма = "+result);
