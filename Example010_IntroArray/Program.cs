16208c18578cf75dab62a00cd1df747d422b7c8b
Михаил Матафонов <Matmix2015@yandex.ru>
2023-02-13 20:20:46 +0300

    add Example010_IntroArray

---
+++ Example010_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int arg1,int arg2,int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result = arg2;
+    if(arg3>result) result = arg3;
+    return result;
+}
+int[] array ={11, 21, 31, 41, 105, 61, 202, 18, 19};
+array[0] = 12;
+Console.WriteLine("Выбранный индекс ="+array[7]);
+int result = Max(
+Max(array[0], array[1], array[2]),
+Max(array[3], array[4], array[5]),
+Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine("Max значение ="+result);
